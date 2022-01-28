72569fe63cf25dcde01f34790f9a8bf4f0db87b8
oleh.kyrychenko <oleh.kyrychenko@tesseris.com>
2022-01-28 23:31:44 +0200

    generation finished

--- GGJ2022/Assets/Scripts/MapGenerator.cs
+++ GGJ2022/Assets/Scripts/MapGenerator.cs
@@ -1,21 +1,23 @@
 using Assets.Scripts;
 using System;
+using System.Text;
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
     public GameObject _darkFloorTile;
     public GameObject _darkSpawn;
-    public string _darkSpawnFloorName;
 
     public GameObject _lightFloorTile;
     public GameObject _lightSpawn;
-    public string _lightSpawnFloorName;
+
+    public GameObject _middleTile;
 
     public int _width;
     public int _length;
 
-    private GameObject[,] _map;
+    public static GameObject[,] _darkMap;
+    public static GameObject[,] _lightMap;
 
     private class FloorSideSpawnInfo
     {
@@ -29,6 +31,7 @@ public class MapGenerator : MonoBehaviour
     private class FloorSideSpawnResult
     {
         public int CountOfTilesInFrontOfSpawn { get; set; }
+        public GameObject[,] OutputMap { get; set; }
     }
 
     //X-axis is left to right
@@ -36,8 +39,6 @@ public class MapGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _map = new GameObject[_length, _width];
-
         //<Scaling light spawn to fir dark spawn size>
         var darkSpawn = Instantiate(_darkSpawn, new Vector3(0, 0, 0), _darkSpawn.transform.rotation, gameObject.transform);
         var lightSpawn = Instantiate(_lightSpawn, new Vector3(0, 0, 0), _lightSpawn.transform.rotation, gameObject.transform);
@@ -64,6 +65,7 @@ public class MapGenerator : MonoBehaviour
             TileCenterY = darkFloorTileBounds.center.y
         };
         var darkFloorSpawnResult = InstantiateFloorForSide(darkFloorSpawnInfo);
+        _darkMap = darkFloorSpawnResult.OutputMap;
 
         //<Instantiation light spawn at its place on the other side of the map>
         var lightSpawnPosition = new Vector3(0, 0, 2 * darkFloorSpawnResult.CountOfTilesInFrontOfSpawn * darkFloorTileBounds.size.z + darkSpawnBounds.size.z);
@@ -82,11 +84,43 @@ public class MapGenerator : MonoBehaviour
             TileCenterY = darkFloorTileBounds.center.y,
             InverseDirection = true
         };
-        InstantiateFloorForSide(lightFloorSpawnInfo);
+        var lightFloorSpawnResult = InstantiateFloorForSide(lightFloorSpawnInfo);
+        _lightMap = lightFloorSpawnResult.OutputMap;
+
+        //<Spawning middle tile decoration>
+        var middleTileRelyFloorTileBounds = _darkMap[0, _darkMap.GetLength(1) / 2].GetComponent<BoxCollider>().bounds;
+        var middleTileLocation = new Vector3(
+            middleTileRelyFloorTileBounds.center.x,
+            middleTileRelyFloorTileBounds.max.y,
+            middleTileRelyFloorTileBounds.max.z
+        );
+        Instantiate(_middleTile, middleTileLocation, Quaternion.identity, gameObject.transform);
+        //<Spawning middle tiledecoration>
+
+        var darkFloorString = GetMapString(darkFloorSpawnResult.OutputMap);
+        var lightFloorString = GetMapString(lightFloorSpawnResult.OutputMap);
+    }
+
+    private string GetMapString(GameObject[,] map)
+    {
+        StringBuilder text = new StringBuilder();
+
+        for(int i = 0; i < map.GetLength(0); ++i)
+        {
+            for(int j = 0; j < map.GetLength(1); ++j)
+            {
+                text.Append(map[i, j] == null ? "0" : "1");
+            }
+            text.Append("\n");
+        }
+
+        return text.ToString();
     }
 
     private FloorSideSpawnResult InstantiateFloorForSide(FloorSideSpawnInfo spawnInfo)
     {
+        var outputMap = new GameObject[spawnInfo.Length, _width];
+
         var tileBounds = spawnInfo.FloorTilePrefab.GetComponent<MeshRenderer>().bounds;
         var spawnBounds = spawnInfo.SpawnObject.GetComponent<BoxCollider>().bounds;
 
@@ -111,10 +145,14 @@ public class MapGenerator : MonoBehaviour
                 float rightSideTileCenterX = scaledSpawnBounds.max.x + (j + 0.5f) * tileBounds.size.x;
 
                 var leftSideTilePosition = new Vector3(leftSideTileCenterX, spawnInfo.TileCenterY, centerZ);
-                Instantiate(spawnInfo.FloorTilePrefab, leftSideTilePosition, Quaternion.identity, gameObject.transform);
+                outputMap[spawnInfo.Length - i - 1, widthAtTheSideOfSpawn - j - 1] = Instantiate(
+                    spawnInfo.FloorTilePrefab, leftSideTilePosition, Quaternion.identity, gameObject.transform
+                );
 
                 var rightSideTilePosition = new Vector3(rightSideTileCenterX, spawnInfo.TileCenterY, centerZ);
-                Instantiate(spawnInfo.FloorTilePrefab, rightSideTilePosition, Quaternion.identity, gameObject.transform);
+                outputMap[spawnInfo.Length - i - 1, widthAtTheSideOfSpawn + countOfTilesBySpawnWidth + j] = Instantiate(
+                    spawnInfo.FloorTilePrefab, rightSideTilePosition, Quaternion.identity, gameObject.transform
+                );
             }
         }
 
@@ -133,13 +171,16 @@ public class MapGenerator : MonoBehaviour
                 float centerZ = zCalculator(i);
 
                 var middleTilePosition = new Vector3(scaledSpawnBounds.min.x + (j + 0.5f) * tileBounds.size.x, spawnInfo.TileCenterY, centerZ);
-                Instantiate(spawnInfo.FloorTilePrefab, middleTilePosition, Quaternion.identity, gameObject.transform);
+                outputMap[spawnInfo.Length - countOfTilesBySpawnLength - i - 1, widthAtTheSideOfSpawn + j] = Instantiate(
+                    spawnInfo.FloorTilePrefab, middleTilePosition, Quaternion.identity, gameObject.transform
+                );
             }
         }
 
         return new FloorSideSpawnResult()
         {
-            CountOfTilesInFrontOfSpawn = countOfTilesInFrontOfSpawn
+            CountOfTilesInFrontOfSpawn = countOfTilesInFrontOfSpawn,
+            OutputMap = outputMap
         };
     }
 
