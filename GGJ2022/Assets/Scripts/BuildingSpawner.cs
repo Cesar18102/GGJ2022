26845dcfb993aee0ed4d8ffc2dde0a5eca28d60f
oleh.kyrychenko <oleh.kyrychenko@tesseris.com>
2022-01-29 18:29:20 +0200

    Player controls and animation

---
+++ GGJ2022/Assets/Scripts/BuildingSpawner.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class BuildingSpawner : MonoBehaviour
+{
+    public GameObject[] _lightBuildings;
+    public GameObject[] _darkBuildings;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
